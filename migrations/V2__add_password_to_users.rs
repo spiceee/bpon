28ca8ae12349d64659532032a212fdaafa9daaef
Fabio Mont Alegre <spiceee@gmail.com>
2024-02-01 21:04:42 -0300

    Add migration

---
+++ migrations/V2__add_password_to_users.rs
@@ -0,0 +1,15 @@
+use barrel::{backend::Pg, types, Migration};
+
+pub fn migration() -> String {
+    let mut m = Migration::new().schema("bpon_dev");
+
+    m.change_table("users", |t| {
+        t.add_column("encrypted_password", types::varchar(255).nullable(true));
+        t.add_column("is_admin", types::boolean().default(false));
+        t.add_column("is_newsletter_subscriber", types::boolean().default(false));
+        t.add_column("created_at", types::datetime().default("now()"));
+        t.add_column("updated_at", types::datetime().default("now()"));
+    });
+
+    m.make::<Pg>()
+}
