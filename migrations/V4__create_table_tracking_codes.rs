2703573dfa25c1ee5c60cb3a30d6b258de622a75
Fabio Mont Alegre <spiceee@gmail.com>
2024-02-02 16:08:31 -0300

    Add tracking_codes migrations

---
+++ migrations/V4__create_table_tracking_codes.rs
@@ -0,0 +1,18 @@
+use barrel::{backend::Pg, types, Migration};
+
+pub fn migration() -> String {
+    let mut m = Migration::new().schema("bpon_dev");
+
+    m.create_table("tracking_codes", |t| {
+        t.add_column("created_at", types::custom("TIMESTAMPTZ").default("now()"));
+        t.add_column("code", types::varchar(13).unique(true).nullable(false)); //NM102246914BR
+        t.add_column("user_id", types::primary());
+        t.add_foreign_key(&["user_id"], "users", &["id"]);
+        t.add_index(
+            "code",
+            types::index(vec!["code"]).unique(true).nullable(false),
+        );
+    });
+
+    m.make::<Pg>()
+}
---
+++ migrations/V5__add_index_user_id_to_tracking_codes.rs
@@ -0,0 +1,12 @@
+use barrel::{backend::Pg, types, Migration};
+
+pub fn migration() -> String {
+    let mut m = Migration::new().schema("bpon_dev");
+
+    m.change_table("tracking_codes", |t| {
+        t.add_column("updated_at", types::custom("TIMESTAMPTZ").default("now()"));
+        t.add_index("user_id", types::index(vec!["user_id"]));
+    });
+
+    m.make::<Pg>()
+}
