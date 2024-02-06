dc2bcf53b9d7cab75c8ddc742b24b2027f7b1be1
Fabio Mont Alegre <spiceee@gmail.com>
2024-02-05 22:33:43 -0300

    Add name to navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
-   return (
-      <nav>
-         <ul>
-            <li>
-               <Link to='/'>Home</Link>
-            </li>
-            <li>
-               <Link to='/about'>About</Link>
-            </li>
-         </ul>
-      </nav>
-   )
-}
+    return (
+        <nav>
+            <ul>
+                <li>Importação Não Autorizada</li>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
 
-export default Navbar
+export default Navbar;
