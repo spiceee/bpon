7d7cfa45c428bab9967b85dbc3cd857ab774070f
Fabio Mont Alegre <spiceee@gmail.com>
2024-02-06 21:16:47 -0300

    Remove user_id index

--- migrations/V4__create_table_tracking_codes.rs
+++ migrations/V4__create_table_tracking_codes.rs
@@ -6,7 +6,7 @@ pub fn migration() -> String {
     m.create_table("tracking_codes", |t| {
         t.add_column("created_at", types::custom("TIMESTAMPTZ").default("now()"));
         t.add_column("code", types::varchar(13).unique(true).nullable(false)); //NM102246914BR
-        t.add_column("user_id", types::primary());
+        t.add_column("user_id", types::custom("BIGSERIAL").nullable(true));
         t.add_foreign_key(&["user_id"], "users", &["id"]);
         t.add_index(
             "code",
