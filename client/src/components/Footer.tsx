d877b8af1f8fdd6e993a510948b03e47d778cc5c
Fabio Mont Alegre <spiceee@gmail.com>
2025-07-17 12:41:22 -0300

    chore: Bump year

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,30 +4,30 @@ import Link from '../components/Link';
 import rustCrab from '../../public/cuddlyferris.png';
 
 const Footer: React.FC = () => {
-	return (
-		<footer>
-			<div className="copyright">
-				© 2024 INA. Made with{' '}
-				<img
-					src={rustCrab}
-					alt="Rust Crab"
-					className="rust-crab"
-					width="32"
-				/>{' '}
-				<Link link="https://www.rust-lang.org/">Rust</Link>. Contato:{' '}
-				<Link link="mailto:ouvidoria@importacaonaoautorizada.com">
-					ouvidoria at importacaonaoautorizada.com
-				</Link>
-				. Twitter:{' '}
-				<Link link="https://twitter.com/ImpNaoAutoriza">
-					@ImpNaoAutoriza
-				</Link>{' '}
-				<span>
-					Este site não é filiado aos Correios ou Receita Federal.
-				</span>
-			</div>
-		</footer>
-	);
+    return (
+        <footer>
+            <div className="copyright">
+                © 2025 INA. Made with{' '}
+                <img
+                    src={rustCrab}
+                    alt="Rust Crab"
+                    className="rust-crab"
+                    width="32"
+                />{' '}
+                <Link link="https://www.rust-lang.org/">Rust</Link>. Contato:{' '}
+                <Link link="mailto:ouvidoria@importacaonaoautorizada.com">
+                    ouvidoria at importacaonaoautorizada.com
+                </Link>
+                . Twitter:{' '}
+                <Link link="https://twitter.com/ImpNaoAutoriza">
+                    @ImpNaoAutoriza
+                </Link>{' '}
+                <span>
+                    Este site não é filiado aos Correios ou Receita Federal.
+                </span>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
